614fa85be73337222b72028936363b0f6fef02e5
Anastasiya <atolochkova@mirantis.com>
2017-04-10 10:54:58 +0400

    Initial Redis Sentinel support
    Change-Id: I9cca2dadfc10a97d800e81a76f7c116c247f9bd0

---
+++ images/redis/run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+function launchmaster {
+  if [[ ! -e $MASTER_DATA ]]; then
+    echo "Redis master data doesn't exist, data won't be persistent!"
+    mkdir $MASTER_DATA
+  fi
+  master_conf=/opt/redis/conf/redis.conf
+  sed -i "s|%data-dir%|$MASTER_DATA|" ${master_conf}
+  redis-server ${master_conf} --protected-mode no
+}
+
+function launchsentinel {
+  while true; do
+    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    if [[ -n ${master} ]]; then
+      master="${master//\"}"
+    else
+      master=$(hostname -i)
+    fi
+    if redis-cli -h ${master} INFO; then
+      break
+    fi
+    echo "Connecting to master failed.  Waiting..."
+    sleep 10
+  done
+  sentinel_conf=/opt/redis/conf/sentinel.conf
+  sed -i "s/%master-ip%/${master}/" ${sentinel_conf}
+  redis-sentinel ${sentinel_conf} --protected-mode no
+}
+
+function launchslave {
+  while true; do
+    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+    master="${master//\"}"
+    if redis-cli -h ${master} INFO; then
+      break
+    fi
+    echo "Connecting to master failed.  Waiting..."
+    sleep 10
+  done
+  data_dir="./"
+  slave_conf=/opt/redis/conf/redis.conf
+  sed -i "s|%data-dir%|${data_dir}|" ${slave_conf}
+  sed -i "s/#//" ${slave_conf}
+  sed -i "s/%master-ip%/${master}/" ${slave_conf}
+  redis-server ${slave_conf} --protected-mode no
+}
+
+if [[ "${SENTINEL}" == "true" ]]; then
+  launchsentinel
+else
+  master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name mymaster | tr ',' ' ' | cut -d' ' -f1)
+  if [[ -n ${master} ]]; then
+    launchslave
+  else
+    launchmaster
+  fi
+fi
