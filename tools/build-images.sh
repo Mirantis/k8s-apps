0749f9c4f5f86d873ecdd8933c86018cbc739dd8
Sergey Reshetnyak <sreshetniak@mirantis.com>
2017-04-27 16:49:37 +0300

    Add scripts for building and pushing images
    Change-Id: I04112c0f439e6e3ef4cc767361e58cb4c8470264

---
+++ tools/build-images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eux
+
+repo_dir="$(dirname "$(dirname "$(readlink -f $0)")")"
+for d in ${repo_dir}/images/*/; do
+    pushd "${d}"
+    image_name="$(basename "${d}")"
+    if [ ! -f "${d}/.version" ]; then
+        echo "Image version is not specified for ${image_name}, skipping..."
+        continue
+    fi
+    version="$(cat ${d}/.version | xargs)"
+    echo "Build ${image_name} image..."
+    docker build -t="mirantisworkloads/${image_name}:${version}" .
+    popd
+done
