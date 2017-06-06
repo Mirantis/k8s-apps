2778da67aeedcc160908268334b33b0e8d418fab
Mikhail <mzhnichkov@mirantis.com>
2017-06-06 17:27:45 +0400

    Implement Cassandra support in Tweeviz
    Change-Id: I512f1f3d74e414fb3abe6b70096e40f6ac408f0c

--- images/tweeviz/setup.py
+++ images/tweeviz/setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='TweeViz',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['tweeviz'],
     include_package_data=True,
     install_requires=[
         'snakebite',
         'flask',
         'click',
+        'cassandra-driver',
     ],
     entry_points='''
         [console_scripts]
--- images/tweeviz/tweeviz.py
+++ images/tweeviz/tweeviz.py
@@ -6,12 +6,15 @@ import threading
 import time
 
 import flask
-from snakebite import client
 
-
+storage = os.getenv('TWEEVIZ_STORAGE', 'hdfs')
+cassandra_address = os.getenv('TWEEVIZ_CASSANDRA_ADDRESS', 'cassandra-cassandra')
+cassandra_keyspace = os.getenv('TWEEVIZ_CASSANDRA_KEYSPACE', 'mirantis')
+cassandra_table = os.getenv('TWEEVIX_CASSANDRA_TABLE', 'tweetics')
 hdfs_address = os.getenv('TWEEVIZ_HDFS_ADDRESS', 'hdfs-namenode')
 hdfs_port = int(os.getenv('TWEEVIZ_HDFS_PORT', 8020))
 results_dir = os.getenv('TWEEVIZ_HDFS_PATH', '/')
+
 min_popularity = int(os.getenv('TWEEVIZ_MIN_POPULARITY', 2))
 top_list_len = int(os.getenv('TWEEVIZ_TOP_LIST_SIZE', 0))
 header = os.getenv('TWEEVIZ_HEADER', 'Twitter stats')
@@ -23,33 +26,43 @@ processed_results = set()
 
 
 def update_stats():
-    hdfs = client.Client(hdfs_address, hdfs_port, use_trash=False)
+    if storage == 'hdfs':
+      from snakebite import client
+      hdfs = client.Client(hdfs_address, hdfs_port, use_trash=False)
+  
+      parts = []
+      for result in sorted([r['path'] for r in hdfs.ls([results_dir])]):
+          if not hdfs.test(result + "/_SUCCESS", exists=True):
+              continue
+          if result in processed_results:
+              continue
+          processed_results.add(result)
+          parts += sorted([r['path'] for r in hdfs.ls([result + "/part*"])])
+  
+      if not parts:
+          return
+  
+      for part in hdfs.text(parts):
+          part_stats = part.split('\n')
+          for stat in part_stats:
+              if not stat:
+                  continue
+              hashtag, count = stat.split(' ')
+              hashtags[hashtag] = hashtags.get(hashtag, 0) + int(count)
+      print "Processed data in: %s" % parts
 
-    parts = []
-    for result in sorted([r['path'] for r in hdfs.ls([results_dir])]):
-        if not hdfs.test(result + "/_SUCCESS", exists=True):
-            continue
-        if result in processed_results:
-            continue
-        processed_results.add(result)
-        parts += sorted([r['path'] for r in hdfs.ls([result + "/part*"])])
-
-    if not parts:
-        return
-
-    for part in hdfs.text(parts):
-        part_stats = part.split('\n')
-        for stat in part_stats:
-            if not stat:
-                continue
-            hashtag, count = stat.split(' ')
-            hashtags[hashtag] = hashtags.get(hashtag, 0) + int(count)
-
+    else:
+      from cassandra.cluster import Cluster
+      cluster = Cluster([cassandra_address])
+      session = cluster.connect(cassandra_keyspace)
+      query = "select * from {}".format(cassandra_table)
+      for row in session.execute(query):
+          hashtags[row.hashtag] = row.count
+  
     sorted_stats = to_jqcloud_format(sorted(hashtags.items(), key=lambda x: x[1], reverse=True))
     max_top = min(len(hashtags), top_list_len)
     stats['popularity'] = sorted_stats if top_list_len == 0 else sorted_stats[:max_top]
 
-    print "Processed data in: %s" % parts
 
 
 def to_jqcloud_format(keypairs):
