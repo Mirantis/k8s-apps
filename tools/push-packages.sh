f040e0527cd59d3ef8da37bda4b618dcd679de4a
Sergey Lukjanov <me@slukjanov.name>
2017-06-08 04:31:48 -0700

    Add env var to set project id
    Change-Id: I4f75f1b16431bc145cd4b36420dcaed7c4a68fd2

--- tools/push-packages.sh
+++ tools/push-packages.sh
@@ -9,11 +9,19 @@ fi
 set -eux
 
 if [ ! -f "${GCLOUD_KEYPATH:-}" ]; then
-    echo "Keyfile not found. Please specify path to keyfile in GCLOUD_KEYPATH environtment variable!"
+    echo "Keyfile not found. Please, specify path to keyfile in GCLOUD_KEYPATH environment variable!"
+    exit 1
+fi
+
+if [ -z "${GCLOUD_PROJECT:-}" ]; then
+    echo "Project id not found. Please, specify project id in GCLOUD_PROJECT environment variable!"
     exit 1
 fi
 
 gcloud auth activate-service-account --key-file "${GCLOUD_KEYPATH}"
+gcloud config set project "${GCLOUD_PROJECT}"
+
 repo_dir="$(dirname "$(dirname "$(realpath $0)")")"
 pushd "${repo_dir}/dist/charts"
+
 gsutil -m rsync ./ gs://${org}/
