c5328e921e850ef5658eeb207d206722c2783d66
Sergey Reshetnyak <sreshetniak@mirantis.com>
2017-07-25 15:20:35 +0300

    Add stages for publish charts in post-merge pipeline
    Change-Id: I412934956c891f4c6fa6d4181ca17703fc23bc0a

--- tools/build-packages.sh
+++ tools/build-packages.sh
@@ -8,13 +8,16 @@ fi
 
 set -eux
 
+HELM_CMD=${HELM_CMD:-helm}
+
 REPO_URL="https://${org}.storage.googleapis.com"
 repo_dir="$(dirname "$(dirname "${PWD}/$0")")"
 
 # build dependencies
 for d in ${repo_dir}/charts/*/; do
     pushd "${d}"
-    helm dep up
+    rm -fr ./charts
+    $HELM_CMD dep up
     popd
 done
 
@@ -23,9 +26,9 @@ packages_dir="${repo_dir}/dist/charts"
 mkdir -p "${packages_dir}"
 pushd "${packages_dir}"
 for d in ${repo_dir}/charts/*/; do
-    helm package "${d}"
+    $HELM_CMD package "${d}"
 done
 
 # generate index
 wget "${REPO_URL}/index.yaml" -O index.yaml
-helm repo index --url "${REPO_URL}" --merge index.yaml .
+$HELM_CMD repo index --url "${REPO_URL}" --merge index.yaml .
