6fbaad92bea3406d886d314c38d085642328b5ce
Sergey Reshetnyak <sreshetniak@mirantis.com>
2017-07-28 17:52:17 +0300

    Add twitter demo nightly pipeline
    Change-Id: Ief58c81b0aaa9f3ea330e0e4c553bde2d1f49e80

---
+++ tools/pipelines/twitter-demo-nightly.groovy
@@ -0,0 +1,37 @@
+def run(helm_home) {
+  def namespace = "twitter-nightly-${env.BUILD_NUMBER}"
+  try {
+    stage("Run tests") {
+      withCredentials([
+        file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG'),
+        string(credentialsId: "twitter-demo-ts-app-key", variable: "TS_APP_KEY"),
+        string(credentialsId: "twitter-demo-ts-app-secret", variable: "TS_APP_SECRET"),
+        string(credentialsId: "twitter-demo-ts-token-key", variable: "TS_TOKEN_KEY"),
+        string(credentialsId: "twitter-demo-ts-token-secret", variable: "TS_TOKEN_SECRET")
+      ]) {
+        withEnv([
+          "HELM_HOME=" + helm_home,
+          "TS_HELM_CMD=" + pwd() + "/helm",
+          "TS_KUBECTL_CMD=" + pwd() + "/kubectl",
+          "TS_NAMESPACE=${namespace}"
+        ]) {
+          sh("./scenarios/twitter-stats/twitter-stats.sh up test down")
+        }
+      }
+    }
+  } finally {
+    stage("Cleanup") {
+      timeout(15) {
+        withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+          def ok = false
+          while (!ok) {
+            sleep(5)
+            ok = sh(script: "./kubectl delete ns --ignore-not-found ${namespace}", returnStatus: true) == 0
+          }
+        }
+      }
+    }
+  }
+}
+
+return this;
