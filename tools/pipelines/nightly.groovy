493b96f7e7c5e7da534f21b72897118d36b55e4d
Sergey Lukjanov <me@slukjanov.name>
2018-02-04 10:51:07 -0800

    Exclude tweepub-fake from jenkins pipelines
    Change-Id: I4f49b2a78ea325297da1c5b1447173037f26bed7

--- tools/pipelines/nightly.groovy
+++ tools/pipelines/nightly.groovy
@@ -17,7 +17,7 @@ def run(helm_home, namespace_prefix, kubernetes_domain) {
           'HELM_CMD=' + pwd() + '/helm',
           'KUBECTL_CMD=' + pwd() + '/kubectl',
         ]) {
-          sh("set -o pipefail; exec 3>&1; go test -v -timeout 90m -args --charts --kubernetes-domain ${kubernetes_domain} --image-repo mirantisworkloads --exclude tweepub,tweetics,tweeviz,tweeviz_api,tweeviz_ui,kibana,logstash,bus-floating-data,monocular --prefix ${namespace_prefix}- 2>&1 3>&- | tee /dev/fd/3 | ./go-junit-report > report.xml 3>&-")
+          sh("set -o pipefail; exec 3>&1; go test -v -timeout 90m -args --charts --kubernetes-domain ${kubernetes_domain} --image-repo mirantisworkloads --exclude tweepub,tweepub-fake,tweetics,tweeviz,tweeviz_api,tweeviz_ui,kibana,logstash,bus-floating-data,monocular --prefix ${namespace_prefix}- 2>&1 3>&- | tee /dev/fd/3 | ./go-junit-report > report.xml 3>&-")
         }
       }
     } finally {
