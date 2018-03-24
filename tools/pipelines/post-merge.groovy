a49eb6c9678162cd7d7eb96b1d123d7d1b3a659e
Andrey Pavlov <apavlov@mirantis.com>
2018-03-24 10:38:33 +0400

    [halyard] Disable testing for now
    Change-Id: I2e31504864ec0664eaa4d7715fa38f6788cd922d

--- tools/pipelines/post-merge.groovy
+++ tools/pipelines/post-merge.groovy
@@ -12,7 +12,7 @@ def run(helm_home, namespace_prefix, kubernetes_domain, buildId, docker_reposito
           'HELM_CMD=' + pwd() + '/helm',
           'KUBECTL_CMD=' + pwd() + '/kubectl',
         ]) {
-          sh("set -o pipefail; exec 3>&1; go test -v -timeout 90m -args --images --charts --kubernetes-domain ${kubernetes_domain} --image-repo ${docker_repository}/${buildId} --build-images-opts='--no-cache' --exclude tweepub,tweepub-fake,tweetics,tweeviz,tweeviz_api,tweeviz_ui,kibana,logstash,bus-floating-data,monocular,rollout --prefix ${namespace_prefix}- 2>&1 3>&- | tee /dev/fd/3 | ./go-junit-report > report.xml 3>&-")
+          sh("set -o pipefail; exec 3>&1; go test -v -timeout 90m -args --images --charts --kubernetes-domain ${kubernetes_domain} --image-repo ${docker_repository}/${buildId} --build-images-opts='--no-cache' --exclude tweepub,tweepub-fake,tweetics,tweeviz,tweeviz_api,tweeviz_ui,kibana,logstash,bus-floating-data,monocular,rollout,halyard --prefix ${namespace_prefix}- 2>&1 3>&- | tee /dev/fd/3 | ./go-junit-report > report.xml 3>&-")
         }
       }
     } finally {
